1deee9e8d44f1d5d1e62a980452680a7acb14796
afeiship <1290657123@qq.com>
2019-07-02 23:43:06 +0800

    add: first build

--- dist/next-nake-rename.js
+++ dist/next-nake-rename.js
@@ -1,3 +1,11 @@
+/*!
+ * name: next-nake-rename
+ * url: https://github.com/afeiship/next-nake-rename
+ * version: 1.0.0
+ * date: 2019-07-02T15:43:01.024Z
+ * license: MIT
+ */
+
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
@@ -18,3 +26,5 @@
     module.exports = nx.nakeRename;
   }
 })();
+
+//# sourceMappingURL=next-nake-rename.js.map
